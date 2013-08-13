e57c2fc6ca2eddae457c6aa493d3ffa5473e42f3
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2013-08-13 23:10:11 +0200

    Add LocalSparkContext

---
+++ src/test/scala/it/unipd/dei/diameter/LocalSparkContext.scala
@@ -0,0 +1,36 @@
+package it.unipd.dei.diameter
+
+import org.scalatest.{BeforeAndAfterEach, Suite}
+import spark.SparkContext
+
+/**
+ * Provides a SparkContext to each test. The SparkContext is initialized and
+ * destroyed before and after each test.
+ */
+trait LocalSparkContext extends BeforeAndAfterEach { self: Suite =>
+
+  var _sc: SparkContext = _
+
+  def sc: SparkContext = _sc
+
+  override def beforeEach() {
+    clearProperties()
+    _sc = new SparkContext("local", "test")
+    super.beforeEach()
+  }
+
+  override def afterEach() {
+    if(_sc != null) {
+      _sc.stop()
+      clearProperties()
+      _sc = null
+    }
+    super.afterEach()
+  }
+
+  private def clearProperties() {
+    System.clearProperty("spark.driver.port")
+    System.clearProperty("spark.hostPort")
+  }
+
+}
