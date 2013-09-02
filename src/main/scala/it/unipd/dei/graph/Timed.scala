4fe8bf76fa5378900191db338d2f9a46c9850199
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2013-09-02 20:53:36 +0200

    Add logging to Timed trait

--- src/main/scala/it/unipd/dei/graph/Timed.scala
+++ src/main/scala/it/unipd/dei/graph/Timed.scala
@@ -17,13 +17,17 @@
 
 package it.unipd.dei.graph
 
+import org.slf4j.LoggerFactory
+
 trait Timed {
+
+  private val timedLogger = LoggerFactory.getLogger("timer")
 
   def timed[R](name: String)(f: => R): R = {
     val start = System.currentTimeMillis()
     val ret = f
     val end = System.currentTimeMillis()
-    println("%s time: %d ms".format(name, (end-start)))
+    timedLogger info ("%s time: %d ms".format(name, (end-start)))
     ret
   }
 
