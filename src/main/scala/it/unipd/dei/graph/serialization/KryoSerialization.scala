d405ab4e37e6c9d2e685a993cafbd6c8ae20fd3d
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2013-09-13 13:38:54 +0200

    Remove all kryo registrations but the one for HyperLogLog counters

--- src/main/scala/it/unipd/dei/graph/serialization/KryoSerialization.scala
+++ src/main/scala/it/unipd/dei/graph/serialization/KryoSerialization.scala
@@ -19,12 +19,6 @@ package it.unipd.dei.graph.serialization
 
 import com.esotericsoftware.kryo.Kryo
 import it.unipd.dei.graph.diameter.hyperAnf.HyperLogLogCounter
-import it.unipd.dei.graph.decompositions.RandomizedBallDecomposition.NodeTag
-import it.unipd.dei.graph.diameter.hyperAnf.HyperLogLogCounter.Register
-import it.unipd.dei.graph.{NodeId, Neighbourhood}
-import spark.{SerializableWritable, Accumulator}
-import com.esotericsoftware.kryo.serializers.JavaSerializer
-import spark.broadcast.HttpBroadcast
 import org.slf4j.LoggerFactory
 
 /**
@@ -45,44 +39,12 @@ class GraphKryoRegistrator extends spark.KryoRegistrator {
   override def registerClasses(kryo: Kryo) {
 
     val toRegister = List(
-      // base types used for the description of graphs
-      1, 1.0, 1.toByte, 1L, true, false, "hello", (0, Array(1,2,3)),
-
-      // ball decomposition types
-      Left(0.toByte), Right(1),
-      (Left(0.toByte), Array(1,2,3)), // nodetag
-      (Right(1), Array(1,2,3)), // nodetag
-      (1, (Left(0.toByte), Array(1,2,3))),
-      (1, (Right(1), Array(1,2,3))),
-      (true, 1), (1, (true, 1)),
-      (1,1),
-      (1, ((Left(0.toByte), Array(1,2,3)), Some(Seq(true, 10)))), // markCandidate data
-      (1, ((Right(1), Array(1,2,3)), Some(Seq(true, 10)))), // markCandidate data
-      (1, ((Right(1), Array(1,2,3)), None)), // markCandidate data
-      (1, ((Left(1.toByte), Array(1,2,3)), None)), // markCandidate data
-      (1, (1,1)), // colorDominated result
-      (1, ((Left(0.toByte), Array(1,2,3)), Some((1,1)))), // applyColors argument
-      (1, ((Right(0), Array(1,2,3)), Some((1,1)))), // applyColors argument
-      (1, ((Right(0), Array(1,2,3)), None)), // applyColors argument
-      (1, ((Left(0.toByte), Array(1,2,3)), None)) // applyColors argument
+      new HyperLogLogCounter(4, 1234)
     )
 
     toRegister.foreach { e =>
-      log debug ("registering class of {}", e.toString)
       log debug ("registering {}", e.getClass)
       kryo.register(e.getClass)
-      val sequence = Seq(e, e, e)
-      log debug ("registering {}", sequence.getClass)
-      kryo.register(sequence.getClass)
-      val list = List(e, e, e)
-      log debug ("registering {}", list.getClass)
-      kryo.register(list.getClass)
-      val arr = Array(e, e, e)
-      log debug ("registering {}", arr.getClass)
-      kryo.register(arr.getClass)
-      val cons = e :: Nil
-      log debug ("registering {}", cons.getClass)
-      kryo.register(cons.getClass)
     }
   }
 
