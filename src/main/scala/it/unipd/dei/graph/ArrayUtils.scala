25e11618879dbca9716a71201c1ad1bde4ebdc90
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2013-12-04 08:45:13 +0100

    Add ArrayUtils.scala
    Contains utilities to deal with arrays, including one to merge sorted arrays

---
+++ src/main/scala/it/unipd/dei/graph/ArrayUtils.scala
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2013 Matteo Ceccarello
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package it.unipd.dei.graph
+
+object ArrayUtils {
+
+  /**
+   * Merge two sorted arrays
+   */
+  def merge(a: Array[NodeId], b: Array[NodeId]): Array[NodeId] = {
+    var i = 0
+    var aIdx = 0
+    var bIdx = 0
+
+    var result: Array[NodeId] = Array.ofDim(a.length)
+
+    // merge the first elements
+    while(aIdx < a.length && bIdx < b.length) {
+      result = ensureCapacity(result, i)
+      if (a(aIdx) == b(bIdx)) {
+        result(i) = a(aIdx)
+        aIdx += 1
+        bIdx += 1
+      } else if(a(aIdx) < b(bIdx)) {
+        result(i) = a(aIdx)
+        aIdx += 1
+      } else {
+        result(i) = b(bIdx)
+        bIdx += 1
+      }
+      i += 1
+    }
+
+    // merge the remaining
+    while(aIdx < a.length) {
+      result = ensureCapacity(result, i)
+      result(i) = a(aIdx)
+      aIdx += 1
+      i += 1
+    }
+    while(bIdx < b.length) {
+      result = ensureCapacity(result, i)
+      result(i) = b(bIdx)
+      bIdx += 1
+      i += 1
+    }
+
+    trim(result, i)
+  }
+
+  private def ensureCapacity(arr: Array[NodeId], i: Int): Array[NodeId] = {
+    if (i < arr.length) {
+      arr
+    } else {
+      val newArr: Array[NodeId] = Array.ofDim(2*arr.length)
+      System.arraycopy(arr, 0, newArr, 0, arr.length)
+      newArr
+    }
+  }
+
+  private def trim(arr: Array[NodeId], i: Int): Array[NodeId] = {
+    if(i<arr.length){
+      val newArr: Array[NodeId] = Array.ofDim(i)
+      System.arraycopy(arr, 0, newArr, 0, i)
+      newArr
+    } else {
+      arr
+    }
+  }
+
+}
